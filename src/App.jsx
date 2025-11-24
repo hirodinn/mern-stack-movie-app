32870f3413f3546cc8cb61692690690e1f04403b
hirodinn <hirebikila1@gmail.com>
2025-11-24 16:49:21 +0300

    setup the end points

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 function App() {
-  return <div className="text-3xl text-blue-600">Hello world</div>;
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    const loadMovies = async () => {
+      try {
+        let popular = await axios.get(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${
+            import.meta.env.VITE_TMDB_KEY
+          }`
+        );
+        setMovies(popular.data.results);
+      } catch (ex) {
+        console.log(ex);
+      }
+    };
+    loadMovies();
+  }, []);
+
+  return (
+    <main>
+      {movies.map((movie, i) => {
+        return <div key={i}>{movie.title}</div>;
+      })}
+    </main>
+  );
 }
 
 export default App;
