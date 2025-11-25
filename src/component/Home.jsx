bb390ee200dbd01d34ca5782521c255184b296d0
hirodinn <hirebikila1@gmail.com>
2025-11-25 10:43:11 +0300

    move the home to its own file to work with routes

---
+++ src/component/Home.jsx
@@ -0,0 +1,69 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import Header from "./Header";
+
+export default function Home() {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    const loadMovies = async () => {
+      try {
+        let popular = await axios.get(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${
+            import.meta.env.VITE_TMDB_KEY
+          }`
+        );
+        setMovies(popular.data.results);
+        console.log(popular.data.results);
+      } catch (ex) {
+        console.log(ex);
+      }
+    };
+    loadMovies();
+  }, []);
+
+  function returnRatingColor(rating) {
+    if (rating >= 8) return "text-blue-700";
+    else if (rating >= 7) return "text-green-700";
+    else if (rating >= 6) return "text-yellow-700";
+    else return "text-red-700";
+  }
+
+  return (
+    <main className="bg-my-black text-white pt-30 h-fit">
+      <Header />
+      <div className="flex flex-wrap justify-evenly w-[90%] max-w-[1200px] mx-auto gap-3 space-y-3">
+        {movies.map((movie, i) => {
+          return (
+            <div
+              key={i}
+              className="w-65 h-155 cursor-pointer bg-my-black-hover flex flex-col p-4 rounded-2xl"
+            >
+              <img
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              />
+              <div className="flex-1">
+                <h4 className="my-2 font-bold text-2xl">
+                  {movie.title.length < 30
+                    ? movie.title
+                    : movie.title.slice(0, 30) + " ..."}
+                </h4>
+                <p>
+                  {movie.overview.length < 100
+                    ? movie.overview
+                    : movie.overview.slice(0, 100) + " ..."}
+                </p>
+              </div>
+              <div
+                className={`${returnRatingColor(
+                  Number(movie.vote_average)
+                )} ml-auto`}
+              >
+                {movie.vote_average}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </main>
+  );
+}
