abc84ac0de709128cd9f705c22b88027af55beef
hirodinn <hirebikila1@gmail.com>
2025-11-25 11:59:19 +0300

    after successful login navigate to the home page

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default function Login({ setIsLogged }) {
+  const navigate = useNavigate();
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -13,6 +16,9 @@ export default function Login({ setIsLogged }) {
     });
     if (user.data) {
       setIsLogged(true);
+      setTimeout(() => {
+        navigate("/home");
+      }, 100);
     }
     console.log(user);
   }
